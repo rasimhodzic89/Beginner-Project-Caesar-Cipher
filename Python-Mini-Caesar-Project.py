31d5e89c67c5b385e72b11ec61ed1a8118207d8b
rasimhodzic89 <rasimhodzic89@gmail.com>
2025-12-15 11:55:44 -0500

    Adding an argument in my caesar function and anddding an or operator to my if statement to check for values between 1 and 25 as associated with the alphabet

--- Python-Mini-Caesar-Project.py
+++ Python-Mini-Caesar-Project.py
@@ -1,8 +1,10 @@
 # Python Mini Caesar Cipher Project
 
-def caesar(text, shift):
+def caesar(text, shift, encrypt=True):
     if not isinstance(shift, int):
-        return "Shift must be an integer value."
+        return "Shift must be an integer valuee."
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
