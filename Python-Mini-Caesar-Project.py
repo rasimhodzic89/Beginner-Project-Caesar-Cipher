754131465efc7fb19b8f2281f7b29d4595b9b9c6
rasimhodzic89 <rasimhodzic89@gmail.com>
2025-12-15 11:31:35 -0500

    I tested the function of Caesar with the two passed arguments in the caesar function

--- Python-Mini-Caesar-Project.py
+++ Python-Mini-Caesar-Project.py
@@ -1,9 +1,11 @@
 # Python Mini Caesar Cipher Project
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-shift = 5
-shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-translation_table = str.maketrans(alphabet, shifted_alphabet)
-text = "hello world"
-encrypted_text = text.translate(translation_table)
-print(encrypted_text)
+def caesar(text, shift):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
+
+encrypted_text = caesar("freeCodeCamp", 3)
+   
